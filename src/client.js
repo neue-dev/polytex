aa02e80a2b060b962fd68f91f2b50d7979330141
modavid <modavid.1964@gmail.com>
2024-11-13 12:58:24 +0800

    Create initial client.

---
+++ src/client.js
@@ -0,0 +1,13 @@
+/**
+ * @ Author: Mo David
+ * @ Create Time: 2024-11-13 12:54:56
+ * @ Modified time: 2024-11-13 13:11:57
+ * @ Description:
+ * 
+ * Main client side file.
+ */
+
+// Set the worker source manually (kinda sad it has to be manual)
+pdfjsLib.GlobalWorkerOptions.workerSrc = 'https://cdnjs.cloudflare.com/ajax/libs/pdf.js/2.6.347/pdf.worker.min.js';
+
+console.log('hello world!')
