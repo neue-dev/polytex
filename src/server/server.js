68044ab6251f6f06f0c172ecc3dbd09ba52022eb
modavid <modavid.1964@gmail.com>
2024-11-14 07:09:35 +0800

    Fix server import + turn package to type module.

--- src/server/server.js
+++ src/server/server.js
@@ -1,14 +1,16 @@
 /**
  * @ Author: Mo David
  * @ Create Time: 2024-11-13 12:35:19
- * @ Modified time: 2024-11-13 12:37:42
+ * @ Modified time: 2024-11-14 06:59:20
  * @ Description:
  * 
  * The main server thread for running the service.
  */
 
 import express from 'express'
-import exressWs from 'express-ws'
+import expressWs from 'express-ws'
+
+import { FS } from './fs.js'
 
 // Create the app and decorate it with WS capabilities
 const app = express()
