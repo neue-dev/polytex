24404479d466f6ec62b6fc02db6839c51426e257
modavid <modavid.1964@gmail.com>
2024-11-13 14:50:57 +0800

    Add PDF rendering through pdfjsLib.

--- src/client.js
+++ src/client.js
@@ -1,7 +1,7 @@
 /**
  * @ Author: Mo David
  * @ Create Time: 2024-11-13 12:54:56
- * @ Modified time: 2024-11-13 13:58:28
+ * @ Modified time: 2024-11-13 14:50:13
  * @ Description:
  * 
  * Main client side file.
@@ -20,8 +20,8 @@ const PolyTeXClient = (() => {
 
 	// Editor, viewer, and renderer
 	const editor = {};
-	const viewer = {};
 	const renderer = {};
+	const compiler = {};
 
 	/**
 	 * Initializes the client.
@@ -31,19 +31,24 @@ const PolyTeXClient = (() => {
 	 */
 	_.init = () => {
 		
-		// Grab the correspnding DOM elements
+		// The editor element and its representation (via Ace)
 		editor.element = document.getElementById('editor');
-		viewer.element = document.getElementById('viewer');
-
-		// The representation (from the Ace library) of the editor
 		editor.model = ace.edit(editor.element)
 
-		// Latex rendering engine
-		renderer.engine = new PdfTeXEngine();
-		renderer.files = {}
+		// The pdf renderer
+		// promisify() is a utility for chaining methods onto the promise returned by getDocument()
+		renderer.element = document.getElementById('viewer');
+		renderer.context = renderer.element.getContext('2d');
+		renderer.document = null;
+		renderer.scale = window.devicePixelRatio || 1;
+		renderer.transform = renderer.scale !== 1 ? [renderer.scale, 0, 0, renderer.scale, 0, 0] : null;
 
-		// Utility for wrapping the extension methods of the renderer
-		renderer.promisify = (f) => () => (renderer.promise.then(f), renderer)
+		// Latex compilation engine
+		// promisify() is another similar utility for chaining methods onto the promise returned by loadEngine()
+		compiler.engine = new PdfTeXEngine();
+		compiler.files = {}
+		compiler.promisify = (f) => (...args) => 
+			(compiler.promise.then(() => f(...args)), compiler)
 
 		// Extend the editor interface
 		Object.assign(editor, {
@@ -65,43 +70,84 @@ const PolyTeXClient = (() => {
 
 		// Extend the renderer
 		Object.assign(renderer, {
+
+			// Renders the provided pdf data and stores it in memory
+			render: (data) => (
+				renderer.promise = pdfjsLib.getDocument({ data }).promise,
+				renderer.promise.then((pdf) => renderer.document = pdf),
+				renderer
+			),
+
+			// Renders a specific page unto the canvas element
+			page: (page, scale=1) => (
+				renderer.promise.then(() => (
+					renderer.document.getPage(page).then((page) => 
+					
+						// Use the viewport to config the canvas element
+						((viewport) => (
+
+							// Update canvas dimensions
+							renderer.element.width = Math.floor(viewport.width * renderer.scale),
+							renderer.element.height = Math.floor(viewport.height * renderer.scale),
+							renderer.element.style.width = Math.floor(viewport.width) + 'px',
+							renderer.element.style.height =  Math.floor(viewport.height) + 'px',
+
+							// Render the page
+							page.render({
+								canvasContext: renderer.context,
+								transform: renderer.transform,
+								viewport: viewport
+							})
+
+						// Grab the viewport of the page
+						))(page.getViewport({ scale }))
+					)
+				))
+			)
+		})
+
+		// Extend the compiler
+		Object.assign(compiler, {
 			
 			// Get-setter for the main file of the document
-			main: renderer.promisify((main) => (
+			main: compiler.promisify((main) => (
 				main 
-					? (renderer.main = main, renderer.engine.setEngineMainFile(main), renderer) 
-					: (renderer.main)
+					? (compiler.main = main, compiler.engine.setEngineMainFile(main), compiler) 
+					: (compiler.main)
 			)),
 
 			// Creates a folder in memory for holding latex files
-			folder: renderer.promisify((folder) => (
-				renderer.engine.makeMemFSFolder(folder)
+			folder: compiler.promisify((folder) => (
+				compiler.engine.makeMemFSFolder(folder)
 			)),
 
 			// Creates a file in memory
-			file: renderer.promisify((file, content) => (
-				renderer.files[file] = content,
-				renderer.engine.writeMemFSFile(file, content)
+			file: compiler.promisify((file, content) => (
+				compiler.files[file] = content,
+				compiler.engine.writeMemFSFile(file, content)
 			)),
 
 			// CLears all the files uploaded to memory
-			clear: renderer.promisify(() => (
-				renderer.files = {},
-				renderer.flushCache()
+			clear: compiler.promisify(() => (
+				compiler.files = {},
+				compiler.flushCache()
 			)),
 
 			// Compiles the latex files, starting from the main file
-			compile: renderer.promisify(() => (
-				renderer.compilation = renderer.engine.compileLaTeX()
+			compile: compiler.promisify(() => (
+				compiler.compilation = compiler.engine.compileLaTeX()
 			)),
 
 			// Shutdown the engine
-			close: renderer.promisify(() => (
-				renderer.engine.closeWorker()
+			close: compiler.promisify(() => (
+				compiler.engine.closeWorker()
 			)),
 
-			// Returns a promise for the result
-			result: () => renderer.compilation
+			// Pipes the result through a function
+			result: (f) => compiler.promisify(() => (
+				compiler.compilation
+					.then(({ pdf, log }) => (console.warn(log), f(pdf), { pdf, log }))
+			))()
 		})
 
 		// Configure the editor
@@ -114,12 +160,48 @@ const PolyTeXClient = (() => {
 			.option('fontSize', '16px')
 			.option('showPrintMargin', false)
 
-		// Configure the renderer
-		renderer.promise = renderer.engine.loadEngine()
-		renderer.promise.then(() => console.log('Renderer is ready.'))
-
-		console.log(renderer.engine)
+		// Configure the compiler
+		compiler.promise = compiler.engine.loadEngine()
+		compiler.promise.then(() => console.log('Renderer is ready.'))
 	}
+
+	/**
+	 * Creates a new folder in the renderer cache.
+	 * 
+	 * @param	folder	The folder name.
+	 * @return				The api.
+	 */
+	_.folder = (folder) => (compiler.folder(folder), _)
+
+	/**
+	 * Creates a new file in the renderer cache.
+	 * If the file exists, it simply overwrite the existing content of that file.
+	 * 
+	 * @param	file			The name of the file.
+	 * @param	content		The content of the file.
+	 * @return					The api.
+	 */
+	_.file = (file, content) => (editor.value(content), compiler.file(file, content), _)
+
+	/**
+	 * Compiles the tex files starting at the provided main file.
+	 * 
+	 * @param main	The main file to use. 
+	 * @return			The api.
+	 */
+	_.compile = (main) => (compiler.main(main).compile(), _)
+
+	/**
+	 * Renders the document unto the canvas.
+	 * If the document does not yet exist, nothing happens.
+	 * 
+	 * @param	page	The page of the document to render. Pages are 1-indexed.
+	 * @return			The api.
+	 */
+	_.render = (page) => (compiler.result((data) => renderer.render(data).page(page)), _)
+
+	// Init the client
+	_.init()
 
 	// Return public interface
 	return {
@@ -128,4 +210,23 @@ const PolyTeXClient = (() => {
 
 })()
 
-PolyTeXClient.init();
+const tex = `
+\\documentclass{article}
+\\usepackage{graphicx} % Required for inserting images
+
+\\title{(PhO)\\^2 2025}
+\\author{Enrico Martinez}
+\\date{June 2024}
+
+\\begin{document}
+
+\\maketitle
+
+\\section{Introduction}
+
+\\end{document}
+`
+PolyTeXClient
+	.file('main.tex', tex)
+	.compile('main.tex')
+	.render(1)
